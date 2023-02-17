78b4b5a193c772bee12ca8f20a7c76f661de5628
Neo <choneo.ch@gmail.com>
2023-02-16 22:35:55 -0800

    buy

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -22,12 +22,27 @@ contract VitaliksHypeShoeStore {
   
     mapping(address => Customer) private customers;
     address[] private customerIndex;
+    
+    event AddCustomer(
+        address indexed customerAddress, 
+        string email, 
+        string cell, 
+        string firstName, 
+        string lastName, 
+        uint256 index
+    );
+
+    event BuyShoe( 
+        address indexed customerAddress, 
+        uint256 id,
+        string productName,
+        uint256 cost
+    );
 
     function isCustomer(address customerAddress) public view returns(bool) {
         if(customerIndex.length == 0) {
             return false;
         }
-
         uint256 tempCustomerIndex = customers[customerAddress].index;
         return customerIndex[tempCustomerIndex] == customerAddress;
     }
@@ -42,5 +57,43 @@ contract VitaliksHypeShoeStore {
 
     function getCustomer(address customerAddress) public view returns(Customer memory) {
         return customers[customerAddress];
+    }
+
+    function addCustomerInformation(
+        address customerAddress, 
+        string memory email, 
+        string memory cell, 
+        string memory firstName, 
+        string memory lastName
+    ) public returns(uint256) {
+        customers[customerAddress].email = email; 
+        customers[customerAddress].cell = cell; 
+        customers[customerAddress].firstName = firstName; 
+        customers[customerAddress].lastName = lastName; 
+        
+        customerIndex.push(customerAddress);
+        customers[customerAddress].index = customerIndex.length-1;
+
+        emit AddCustomer(
+            customerAddress, 
+            customers[customerAddress].email, 
+            customers[customerAddress].cell, 
+            customers[customerAddress].firstName, 
+            customers[customerAddress].lastName, 
+            customers[customerAddress].index
+        );
+
+        return customerIndex.length-1;
+    }
+
+    function buyShoe(
+        address customerAddress, 
+        uint256 id, 
+        string memory productName, 
+        uint256 cost
+    ) public {
+        require(!isCustomer(customerAddress), "Customer needs to register first!");
+        customers[customerAddress].purchasedItems.push(Item({id: id, productName: productName, cost: cost}));
+        emit BuyShoe(customerAddress, id, productName, cost);
     }
 }
